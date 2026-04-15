b591a6be1d64340e89051194e9bd6b475c2bf3bc
YeeFong21 <23004901@siswa.um.edu.my>
2026-04-16 00:27:42 +0800

    Delete push-background.bat

--- push-background.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "C:\Users\User\Documents\Claude\Projects\my-counseling-site"
-del /f ".git\index.lock" 2>nul
-git add -A
-git commit -m "add global interactive animated background - floating blobs, mouse tracking, scroll parallax"
-git push origin main
-echo.
-echo Done! Check Vercel for deployment.
-pause
